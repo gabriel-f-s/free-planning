27a4185f37336319da167c13df2565ee5f4f5c46
gabriel-f-s <gabriel.fonseca-dev@outlook.com>
2026-04-16 10:58:17 -0300

    adding the first files

---
+++ src/test/java/com/atlasys/freeplanning/StartupTests.java
@@ -0,0 +1,13 @@
+package com.atlasys.freeplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
